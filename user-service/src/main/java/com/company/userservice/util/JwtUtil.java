a65a0754aef2ca293b706e2486c5f4f6035b2248
Bobbyshu <bobbyshu0824@gmail.com>
2026-02-03 16:38:11 -0500

    chore(): setup security dependencies, password encoder and jwt util

---
+++ user-service/src/main/java/com/company/userservice/util/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.company.userservice.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+    // secret key
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    // generate Token
+    public String generateToken(String username, String role, String membershipLevel) {
+        Map<String, Object> claims = Map.of(
+                "role", role,
+                "membership_level", membershipLevel
+        );
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10小时
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
