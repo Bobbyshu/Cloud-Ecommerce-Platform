97b1916273c5f1dc543883b62775e6d5b41b48b7
Bobbyshu <bobbyshu0824@gmail.com>
2026-02-02 20:59:06 -0500

    init: spring cloud ecommerce bootstrap

---
+++ src/main/java/com/company/cloudecommerceplatform/CloudEcommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.company.cloudecommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEcommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEcommercePlatformApplication.class, args);
+    }
+
+}
