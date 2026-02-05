d673d8249945c2b23594517dea9e63fbf7074492
Bobbyshu <bobbyshu0824@gmail.com>
2026-02-05 13:43:53 -0500

    feat(product-service): add exception

---
+++ order-service/src/main/java/com/company/orderservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.company.orderservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFound(ProductNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(InsufficientStockException.class)
+    public ResponseEntity<String> handleInsufficientStock(InsufficientStockException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    // for Feign fail
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Server error: " + e.getMessage());
+    }
+}
---
+++ order-service/src/main/java/com/company/orderservice/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.company.orderservice.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(Long productId) {
+        super("Product with ID: " + productId + "not exist");
+    }
+}
